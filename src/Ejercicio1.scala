2e92f4e19cd729a0af6029012e0baeb400bf9d46
Cristofer32513 <cristofer32513@gmali.com>
2021-03-03 18:11:48 -0600

    Ejercicio1 completado.

---
+++ src/Ejercicio1.scala
@@ -0,0 +1,43 @@
+import scala.collection.mutable.ListBuffer
+import scala.io.StdIn.{readInt, readLine}
+
+object Ejercicio1 {
+
+  def llenarLista(palabras:ListBuffer[String]) : Unit = {
+    println("¿Cuantas palabras va a ingresar?")
+    val cantidadPalabras = readInt()
+
+    for (i <- 1 to cantidadPalabras) {
+      println("Ingrese la palabra #" + i + "...")
+      palabras += readLine()
+    }
+  }
+
+  def palabraABuscar() : String = {
+    println("\n¿Que palabra queire buscar en la lista de palabras?")
+    val palabra = readLine()
+
+    palabra
+  }
+
+  def mostrarLista(lista:ListBuffer[String]) : Unit = {
+    println()
+    print("[ ")
+    for (e <- lista.indices) if(e == lista.length-1) print(lista(e)) else print(lista(e) + ", ")
+    println(" ]")
+  }
+
+  def buscarPalabra(lista:ListBuffer[String], palabra:String) : Int = {
+    var cantidadCoincidencias = 0
+    for (e <- lista) if(e.equals(palabra)) cantidadCoincidencias+=1
+    mostrarLista(lista)
+
+    cantidadCoincidencias
+  }
+
+  def main(args: Array[String]): Unit = {
+    val palabras = new ListBuffer[String]()
+    llenarLista(palabras)
+    println("Se encontraron " + buscarPalabra(palabras, palabraABuscar()) + " coincidencias en la lista.")
+  }
+}
