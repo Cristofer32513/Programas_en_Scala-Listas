28f4c1247c77db2b03b6a3d7fbe27e9d26f8aaf8
Cristofer32513 <cristofer32513@gmali.com>
2021-03-03 19:38:52 -0600

    Ejercicio4 completado.

---
+++ src/Ejercicio4.scala
@@ -0,0 +1,52 @@
+import scala.collection.mutable.ListBuffer
+import scala.io.StdIn.{readInt, readLine}
+
+object Ejercicio4 {
+
+  def llenarLista(palabras:ListBuffer[String]) : Unit = {
+    println("¿Cuantas palabras va a ingresar?")
+    val cantidadPalabras = readInt()
+
+    for (i <- 1 to cantidadPalabras) {
+      println("Ingrese la palabra #" + i + "...")
+      palabras += readLine()
+    }
+  }
+
+  def mostrarLista(lista:ListBuffer[String]) : Unit = {
+    print("[ ")
+    for (e <- lista.indices) if(e == lista.length-1) print(lista(e)) else print(lista(e) + ", ")
+    println(" ]")
+  }
+
+  def actualizarLista(lista1:ListBuffer[String], lista2:ListBuffer[String]) : Unit = {
+    for (e <- lista2.indices) {
+      for (j <- lista1.indices) {
+        if(lista1(j).equals(lista2(e))) {
+          lista1.remove(j)
+          actualizarLista(lista1, lista2)
+          return
+        }
+      }
+    }
+
+    println("\nLista actualizada")
+    mostrarLista(lista1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    print("Palabras de la lista 1, ")
+    val palabras1 = new ListBuffer[String]()
+    llenarLista(palabras1)
+
+    println()
+    print("Palabras de la lista 2, ")
+    val palabras2 = new ListBuffer[String]()
+    llenarLista(palabras2)
+
+    println("\nListas originales")
+    mostrarLista(palabras1)
+    mostrarLista(palabras2)
+    actualizarLista(palabras1, palabras2)
+  }
+}
